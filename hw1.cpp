a77bbd8ad8d708bf70a21d2db6a54c93e9703080
Andrew Hurle <fracture91@gmail.com>
2013-01-22 01:38:24 -0500

    Read GRS file and parse out comments

--- hw1.cpp
+++ hw1.cpp
@@ -6,6 +6,7 @@
 // Angel.h is homegrown include file that also includes glew and freeglut
 
 #include "Angel.h"
+#include "textfile.cpp"
 
 // Number of points in drawing
 const int NumPoints = 15000;
@@ -158,6 +159,41 @@ main( int argc, char **argv )
 	// add mouse handler
 	// add resize window functionality (should probably try to preserve aspect ratio)
 
+	char* file = textFileRead("drawings/vinci.dat");
+	char* endOfFile = file + strlen(file);
+
+	// find the first line starting with an asterisk
+	// this indicates end of comment block
+	char* firstStar = strstr(file, "\n*");
+	if(firstStar != NULL) {
+		// pull out comment and print it, just because
+		size_t commentLen = firstStar - file;
+		char* comment = (char*)malloc(sizeof(char) * (commentLen + 1));
+		strncpy(comment, file, commentLen);
+		comment[commentLen] = '\0';
+		cout << "Comment:" << endl << comment << endl;
+
+		char* dataStart = strstr(firstStar + 1, "\n");
+		if(dataStart == NULL) {
+			cout << "No data" << endl;
+			exit(1);
+		}
+		dataStart += 1; // point after newline
+		
+		// copy data into new memory
+		size_t dataLen = endOfFile - dataStart;
+		char* data = (char*)malloc(sizeof(char) * (dataLen + 1));
+		strncpy(data, dataStart, dataLen);
+		data[dataLen] = '\0';
+		free(file); // get rid of old file
+		file = data;
+	} else {
+		cout << "No comment" << endl;
+	}
+
+	cout << file << endl;
+
+	fflush(stdout);
 	// enter the drawing loop
 	// frame rate can be controlled with 
 	glutMainLoop();
